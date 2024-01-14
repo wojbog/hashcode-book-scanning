fb0b5b441a8378029ebc8d2d21da97e2559e10cd
shhhQuiettt <krzysztof.skro@gmail.com>
2024-01-14 16:28:46 +0100

    nf lib

--- score_result.py
+++ score_result.py
@@ -2,7 +2,8 @@ from library import Library
 
 
 def print_solution(libraries: list[Library], order_of_signup: list[int]):
-    print(str(len(order_of_signup)))
+    # print(str(len(order_of_signup)))
+    print(len(order_of_signup))
     for library_id in order_of_signup:
         print(f"{library_id} {len(libraries[library_id].assignments)}")
         libraries[library_id].print_assignments()
